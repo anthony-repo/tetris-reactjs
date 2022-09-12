415ab9ce8041715f4eb261ab706f3e0b96bc539d
Anthony <anthonyshao@gmail.com>
2022-09-12 01:26:07 -0700

    updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Deployment on Firebase
+
+https://tetris-reactjs-35140.web.app/
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -6,14 +6,19 @@ const buildPlayer = (previousPlayer) => {
     let tetrominoes;
     
     if (previousPlayer) {
+        //tetrominoes works like a queue,
+        //unshift will add a new random tetromino to the end
+        //of the player's tetrominoes
         tetrominoes = [...previousPlayer.tetrominoes];
         tetrominoes.unshift(randomTetromino());
     } else {
+        //player is initialized with 4 tetrominoes
         tetrominoes = Array(4)
             .fill(0)
             .map((_) => randomTetromino());
     }
 
+    //spawns a new tetromino on the board
     return {
         collided: false,
         isFastDropping: false,
@@ -24,6 +29,7 @@ const buildPlayer = (previousPlayer) => {
 };
 
 export const usePlayer = () => {
+    //initializing a player
     const [player, setPlayer] = useState(buildPlayer());
 
     const resetPlayer = useCallback(() => {
